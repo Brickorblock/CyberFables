7e5cc66a4883d19c5f49eadb74906b342c409c8a
Joshua Huynh <z5214480@ad.unsw.edu.au>
2020-07-22 18:45:59 +1000

    (WIP) added button on LittleredResult1Fragment

--- app/src/main/java/com/example/cyberfables/interactives/LittleredResult1Fragment.kt
+++ app/src/main/java/com/example/cyberfables/interactives/LittleredResult1Fragment.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.cyberfables.R
 import kotlinx.android.synthetic.main.fragment_littlered_interactive1.*
+import kotlinx.android.synthetic.main.fragment_littlered_result1.*
 import kotlinx.android.synthetic.main.fragment_littlered_result1.view.*
 
 class LittleredResult1Fragment() : Fragment() {
 
     private var correctChoice: Boolean = false
+    private lateinit var nextButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,7 @@ class LittleredResult1Fragment() : Fragment() {
         val root = inflater.inflate(R.layout.fragment_littlered_result1, container, false)
         
         val pageImage = root.pageImage
+        nextButton = root.nextButton
 
         if (correctChoice) {
             pageImage.setImageResource(R.drawable.littlered_3_decision1_correct)
@@ -35,8 +40,20 @@ class LittleredResult1Fragment() : Fragment() {
             pageImage.setImageResource(R.drawable.littlered_3_decision1_incorrect)
         }
 
+
+
         // Inflate the layout for this fragment
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // play animations
+        val buttonAnim = AnimationUtils.loadAnimation(context, R.anim.fade_in_slow);
+        buttonAnim.startTime = AnimationUtils.currentAnimationTimeMillis() + 2000
+        nextButton.animation = buttonAnim
+
     }
 
 }
