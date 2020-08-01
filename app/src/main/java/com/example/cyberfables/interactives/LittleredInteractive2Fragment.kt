6a2c221a9eb59287ddb0e8e47b7d8cc839795fca
Immanuel Abosh <immanuelabosh@outlook.com>
2020-08-01 17:48:50 +1000

    little red fable working fully

--- app/src/main/java/com/example/cyberfables/interactives/LittleredInteractive2Fragment.kt
+++ app/src/main/java/com/example/cyberfables/interactives/LittleredInteractive2Fragment.kt
@@ -1,5 +1,6 @@
 package com.example.cyberfables.interactives
 
+import android.R.color
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
@@ -10,7 +11,6 @@ import android.view.View
 import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
@@ -18,17 +18,8 @@ import com.bumptech.glide.Glide
 import com.example.cyberfables.Helper
 import com.example.cyberfables.MainActivity
 import com.example.cyberfables.R
-import com.example.cyberfables.entities.Fable
-import kotlinx.android.synthetic.main.fragment_book_detail.view.*
-import kotlinx.android.synthetic.main.fragment_littlered_interactive1.*
-import kotlinx.android.synthetic.main.fragment_littlered_interactive1.hitboxes
-import kotlinx.android.synthetic.main.fragment_littlered_interactive1.view.*
 import kotlinx.android.synthetic.main.fragment_littlered_interactive2.*
-import kotlinx.android.synthetic.main.fragment_littlered_interactive2.view.*
-import kotlinx.android.synthetic.main.fragment_littlered_result1.view.*
-import kotlinx.android.synthetic.main.fragment_storypage.*
 import kotlinx.android.synthetic.main.fragment_storypage.view.*
-import kotlinx.android.synthetic.main.fragment_storypage.view.pageImage
 
 
 class LittleredInteractive2Fragment : Fragment() {
@@ -54,21 +45,21 @@ class LittleredInteractive2Fragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
-        Glide.with(hitboxes.context)
-            .load(R.drawable.littlered_6_decision2_hitboxes)
-            .dontAnimate()
-            .thumbnail(0.1f)
-            .into(hitboxes)
         Glide.with(pageImage.context)
             .load(R.drawable.littlered_6_decision2)
             .dontAnimate()
             .thumbnail(0.1f)
             .into(pageImage)
 
+        Glide.with(hitbox.context)
+            .load(R.drawable.littlered_6_decision2_hitboxes)
+            .into(hitbox)
+
+
 
         // handles custom hitbox touch zones on a static image.
         // See Helper.getHitboxColour() for more details
-        hitboxes.setOnTouchListener(OnTouchListener { v, ev ->
+        hitbox.setOnTouchListener(OnTouchListener { v, ev ->
             val action = ev.action
             val evX = ev.getX().toInt()
             val evY = ev.getY().toInt()
@@ -76,7 +67,7 @@ class LittleredInteractive2Fragment : Fragment() {
             Log.d("LittleredInteractive", "evX = $evX, evY = $evY")
 
             if (action == MotionEvent.ACTION_UP) {
-                val pixelColour: Int = Helper().getHitboxColour(hitboxes, evX, evY)
+                val pixelColour: Int = Helper().getHitboxColour(hitbox, evX, evY)
                 Log.d("LittleredInteractive", "pixelColour = $pixelColour")
 
                 determineChoice(pixelColour)
@@ -90,24 +81,21 @@ class LittleredInteractive2Fragment : Fragment() {
         var choiceMade = false
         lateinit var choiceBundle: Bundle
 
-        when (pixelColour) {
-            Color.WHITE -> Toast.makeText(
+        val red = pixelColour shr 16 and 0xff
+        val blue = pixelColour and 0xff
+
+        if (pixelColour == Color.WHITE){
+            Toast.makeText(
                 context,
-                "Tap on the profile you'd like to add!",
+                "Make a decision on the phone!",
                 Toast.LENGTH_SHORT
             ).show()
-
-            // correct choice
-            Color.BLUE -> {
-                choiceBundle = bundleOf(KEY to true)
-                choiceMade = true
-            }
-
-            // incorrect choice
-            Color.RED -> {
-                choiceBundle = bundleOf(KEY to false)
-                choiceMade = true
-            }
+        }else if (red > 100){
+            choiceBundle = bundleOf(KEY to false)
+            choiceMade = true
+        }else if (blue > 100){
+            choiceBundle = bundleOf(KEY to true)
+            choiceMade = true
         }
 
         //navigate to results based on choice
